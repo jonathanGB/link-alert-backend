63b9011a4ca598a352289746458eac4ba0cea0d4
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-03-05 15:29:14 -0500

    test typeof parameter

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req, res) {
-	req.query.list ?
-		res.send(req.query.list):
+app.get('/gettingSourceLinks', function(req, res) {
+	if (req.query.list)
+		res.send(typeof req.query.list);
+	else
 		res.send("noooooope");
 });
 
