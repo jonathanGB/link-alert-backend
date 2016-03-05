f601a2d2f02d06c7de98099ebaa6cef639741056
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-03-05 16:48:57 -0500

    get source link test

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var app = express();
+var http = require('follow-redirects').http;
+var https = require('follow-redirects').https;
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/gettingSourceLinks', function(req, res) {
-	if (req.query.list)
-		res.send(typeof req.query.list);
+	if (req.query.list) {
+		var list = JSON.parse(req.query.list);
+		var returnedList = [];
+
+		list.forEach(function(url) {
+			returnedList.push(getSourceLink(url));
+		});
+
+		res.send({list: returnedList});
+	}
 	else
-		res.send("noooooope");
+		res.sendStatus(404);
 });
 
 app.listen(app.get('port'));
+
+
+function isHttps(url) {
+	return /^https/.test(url);
+}
+
+
+function getSourceLink(url) {
+	var sourceLink = '';
+
+	var protocol = isHttps(url) ?
+		https :
+		http;
+
+	protocol.get(url, function(res) {
+		sourceLink = res.fetchedUrls[0];
+	});
+
+	return sourceLink;
+}
