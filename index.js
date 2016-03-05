83e6783bb358e58ca730422665a75482e9ccf4fd
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-03-05 13:36:49 -0500

    fix parameter name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
+app.get('/', function(req, res) {
   req.xhr ?
   res.send("ajax baby"):
   res.send("nope nope nope");
