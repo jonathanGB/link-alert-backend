c15b2937d34beb79b3a21f9d2b6c285110bbf3f0
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-03-05 13:35:08 -0500

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send('wazzup');
+  req.xhr ?
+  res.send("ajax baby"):
+  res.send("nope nope nope");
 });
 
 app.listen(app.get('port'));
