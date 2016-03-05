999c0f7766bb6639fa4de314abd7f82d153d3572
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-03-05 15:04:33 -0500

    test index

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
-  req.xhr ?
-  res.sendStatus(200):
-  res.sendStatus(202);
+	res.send('looooooooool');
 });
 
 app.listen(app.get('port'));
