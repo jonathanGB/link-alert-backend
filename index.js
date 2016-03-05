afe763b2ac2a097b0233238f8d30972a1d78a25d
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-03-05 17:02:13 -0500

    test source url 2

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/gettingSourceLinks', function(req, res) {
 	if (req.query.list) {
 		var list = JSON.parse(req.query.list);
-		res.send(list[0]);
-		// var returnedList = [];
+		var returnedList = [];
 
-		// list.forEach(function(url) {
-		// 	returnedList.push(getSourceLink(url));
-		// });
+		list.forEach(function(url) {
+			returnedList.push(getSourceLink(url));
+		});
 
-		// res.send({list: returnedList});
+		res.send({list: returnedList});
 	}
 	else {
 		res.sendStatus(404);
